636bbcf8c7a526b0383e9876bfcd68971ffb0b5a
Mike <chiendau1998@gmail.com>
2024-04-27 03:10:33 +0700

    fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 
-import Loader from './common/Loader';
-import Routes from './routes';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
 import AppProvider from './store';
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(true);
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
-
-  if (loading) return <Loader />;
-
   return (
     <AppProvider>
       <Router>
