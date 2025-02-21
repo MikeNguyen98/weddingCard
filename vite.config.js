ecc194dbeec75581662d511e22722cba81f8446a
Mike <chiendau1998@gmail.com>
2025-02-21 14:51:20 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/cdn\.yoursite\.com\//,
-            handler: 'CacheFirst',
-            options: { cacheName: 'cdn-assets' },
-          },
-        ],
-      },
-    }),
   ],
   server: {
     open: true,
